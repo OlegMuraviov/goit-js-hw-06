c48afa06d670c1b6bc3a6aca0c131f2d824194ac
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-02-22 14:57:46 +0200

    fixed

--- js/task-08.js
+++ js/task-08.js
@@ -1,40 +1,37 @@
 // Напиши скрипт управления формой логина.
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением
+// о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму,
+// собери значения полей в обьект, где имя поля будет именем свойства,
+// а значение поля - значением свойства.
+// Выведи обьект с введенными данными в консоль и очисти значения
+// полей формы методом reset.
+//проверка валидности введенных данных
 
 const formEl = document.querySelector(".login-form");
 const inputEl = document.querySelectorAll("input");
 
-// Обработка отправки формы form.login-form должна быть по событию submit.
 formEl.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
-  // При отправке формы страница не должна перезагружаться.
   event.preventDefault();
-  // Если в форме есть незаполненные поля, выводи alert с предупреждением
-  // о том, что все поля должны быть заполнены.
-  const elementData = event.currentTarget.elements;
-  formInputChecker(elementData.email.value, elementData.password.value);
 
-  // Если пользователь заполнил все поля и отправил форму,
-  // собери значения полей в обьект, где имя поля будет именем свойства,
-  // а значение поля - значением свойства.
-  const email = elementData.email.value;
-  const password = elementData.password.value;
   const formInputValues = {
-    email,
-    password,
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
   };
-  // Выведи обьект с введенными данными в консоль и очисти значения
+
+  formInputChecker(formInputValues);
+
   console.log(formInputValues);
-  // полей формы методом reset.
   event.currentTarget.reset();
 }
 
-//проверка валидности введенных данных
-function formInputChecker(email, password) {
-  const messageEmail =
-    "Вы не ввели email! Также Вы можете поддержать нас донатом";
-  const messagePSW =
-    "Вы не ввели пароль! Также Вы можете поддержать нас донатом";
+function formInputChecker({ email, password }) {
+  const messageEmail = "Вы не ввели email!";
+  const messagePSW = "Вы не ввели пароль!";
   if (email === "") return `${alert(messageEmail)}`;
   if (password === "") return `${alert(messagePSW)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,50 +1,44 @@
+/* Напиши скрипт создания и очистки коллекции элементов.
+Пользователь вводит количество элементов в input и нажимает кнопку Создать,
+после чего рендерится коллекция.
+При нажатии на кнопку Очистить, коллекция элементов очищается.
+Размеры самого первого <div> - 30px на 30px.
+Создай функцию createBoxes(amount), которая принимает один параметр - число.
+Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
+Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую
+функцию getRandomHexColor для получения цвета.
+Каждый элемент после первого, должен быть шире и выше предыдущего на 10px. */
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// Напиши скрипт создания и очистки коллекции элементов.
 
-// Пользователь вводит количество элементов в input и нажимает кнопку Создать,
-// после чего рендерится коллекция.
 const controlsEl = document.querySelector("#controls");
 const inputCountEl = document.querySelector("input");
 const buttonCreateEl = document.querySelector("[data-create]");
 const buttonDestroyEl = document.querySelector("[data-destroy]");
 const boxesEl = document.querySelector("#boxes");
 
-// При нажатии на кнопку Очистить, коллекция элементов очищается.
-inputCountEl.addEventListener("input", onInputValue);
-buttonDestroyEl.addEventListener("click", onDestroyBoxes);
 buttonCreateEl.addEventListener("click", onCreateBoxes);
+buttonDestroyEl.addEventListener("click", onDestroyBoxes);
 
-let inputCurrentValue = 0;
-// Размеры самого первого <div> - 30px на 30px.
 let size = 30;
 
-function onInputValue(event) {
-  inputCurrentValue = event.currentTarget.value;
-  console.log(`value = ${inputCurrentValue}`);
-}
-function onDestroyBoxes() {
-  inputCurrentValue = 0;
-  inputCountEl.value = "";
-  boxesEl.innerHTML = "";
-}
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число.
-// Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
-function onCreateBoxes(event) {
+function onCreateBoxes() {
   const amount = inputCountEl.value;
   inputCountEl.value = "";
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
-    // Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую
-    // функцию getRandomHexColor для получения цвета.
     box.style.backgroundColor = getRandomHexColor();
-    // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
     size += 10;
-    console.log(boxesEl);
     boxesEl.appendChild(box);
   }
+}
+
+function onDestroyBoxes() {
+  size = 30;
+  inputCountEl.value = "";
+  boxesEl.innerHTML = "";
 }
